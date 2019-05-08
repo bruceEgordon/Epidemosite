2d9fd2ee7613f917b7b1c319351dbd4140763795
Bruce Gordon <bruce@focusls.com>
2019-05-08 09:05:14 -0400

    1

---
+++ Epidemosite/Business/Initialization/RegisterDependencyResolverInitializationModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Epidemosite.Business.SiteDependencyResolver;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace Epidemosite.Business.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class RegisterDependencyResolverInitializationModule : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            //DependencyResolver.SetResolver(new ServiceLocatorDependencyResolver(context.StructureMap()));
+            
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+            DependencyResolver.SetResolver(new ServiceLocatorDependencyResolver(context.Locate.Advanced));
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+    }
+}
---
+++ Epidemosite/Controllers/EditorialBlockController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Epidemosite.Models.Blocks;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+using EPiServer.Web.Routing;
+
+namespace Epidemosite.Controllers
+{
+    public class EditorialBlockController : BlockController<EditorialBlock>
+    {
+        private IPageRouteHelper _pageRouteHelper;
+        public EditorialBlockController(IPageRouteHelper pageRouteHelper)
+        {
+            _pageRouteHelper = pageRouteHelper;
+        }
+        public override ActionResult Index(EditorialBlock currentBlock)
+        {
+            var hostingPage = _pageRouteHelper.Page;
+            ViewBag.pageName = hostingPage.Name;
+            return PartialView(currentBlock);
+        }
+    }
+}
